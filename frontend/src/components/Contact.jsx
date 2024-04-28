09fb7620290127a6e97df8f9788b0e9fcbfe062c
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-28 12:43:30 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -2,6 +2,7 @@ import  { useEffect, useRef } from 'react';
 import profileImage1 from '../assets/jitendraprofilephoto.jpg';
 import Rating from '../components/Rating';
 import Review from '../components/Review';
+import benefit from '../styles/benefit.css';
 
 import styled, { keyframes } from 'styled-components';
 import { motion, AnimatePresence,  } from "framer-motion";
